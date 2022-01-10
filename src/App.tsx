f845676efd89e35d294105cc77be39159bc7bb43
zhangtao615 <295564283@qq.com>
2022-01-10 21:16:55 +0800

    Menu组件部分功能开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0} mode='vertical' onSelect={(index) => { console.log(index); }}>
+          <MenuItem>
+            Link1
+          </MenuItem>
+          <MenuItem disabled>
+            Link2
+          </MenuItem>
+          <MenuItem>
+            Link3
+          </MenuItem>
+        </Menu>
         <Button onClick={(e) => {e.preventDefault(); alert(123)}}> Hello </Button>
         <Button className="custom" disabled> Disabled Button </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
